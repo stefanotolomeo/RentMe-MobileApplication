ec83583d33c7fbac18059277f3324bad1f9495ed
stefanotolomeo <ste.tolomeo@gmail.com>
2015-08-03 01:01:12 +0200

    setting

--- www/js/home.js
+++ www/js/home.js
@@ -26,13 +26,66 @@ function getUrlParameters(parameter, staticURL, decode){
 
 $(document).ready(loadStuff);
 
+var count=0;
+var comparsoPrimo=true;
+
 function loadStuff(){
     $(".selected").css("color","blue");
+
+    $("#openSettingMenu").click(function() {
+
+        if(count%2==0){
+            $('.settingMenu').animate({ marginLeft: '-15%' , opacity: 1 }, 500);
+            $(".centralContent").fadeTo( "slow" , 0.2);
+            $("#bottomNavbar").fadeTo( "slow" , 0);
+            $("#bottomNavbar").addClass("notClickable");
+
+            comparsoPrimo=true;
+
+        }
+        else{
+            $('.settingMenu').animate({ marginLeft: '1em' , opacity: 1 }, 1000);
+            $( ".centralContent" ).fadeTo( "slow" , 1);
+            $( "#bottomNavbar" ).fadeTo( "slow" , 1);
+            $("#bottomNavbar").removeClass("notClickable");
+
+
+            comparsoPrimo=false;
+        }
+
+        count++;
+    });
+
+    if(comparsoPrimo==true){
+            $(".centralContent").click(function (){
+                $('.settingMenu').animate({ marginLeft: '1em' , opacity: 1 }, 1000);
+                $( ".centralContent" ).fadeTo( "slow" , 1);
+                $( "#bottomNavbar" ).fadeTo( "slow" , 1);
+                $("#bottomNavbar").removeClass("notClickable");
+                count++;
+                comparsoPrimo=false;
+            });
+
+            $(".settingList").click(function(){
+                // RIMUOVE GLI EVIDENZIATI
+                $(".settingList").removeClass("settingListBolder");
+
+                // EVIDENZIA QUELLO CLICCATO
+                $(this).addClass("settingListBolder");
+                 $('.settingMenu').animate({ marginLeft: '-40%' , opacity: 1 }, 500);
+            });
+    }
+        //TESTO MODIFICABILE :)
+//            $('button').click(function(){
+//    var $div=$('div'), isEditable=$div.is('.editable');
+//    $('div').prop('contenteditable',!isEditable).toggleClass('editable')
+}
+
 
     // Non riesco a passare il parametro da index.js
     // Per il momento non lo passo e fisso io un valore per fare delle prove
     //var idClient =  getUrlParameters("id", "", true);
-
+/*
     var idClient=3;
     //console.log(idClient);
     $.ajax({
@@ -54,7 +107,8 @@ function loadStuff(){
             console.log("Error");
         }
     });
-}
+
+    */
 
 
 
