a80861f65450c098f521d5ec8911bafaceef9ae6
stefanotolomeo <ste.tolomeo@gmail.com>
2015-08-04 13:16:00 +0200

    impostazioni quasi finito

--- www/js/home.js
+++ www/js/home.js
@@ -34,10 +34,12 @@ function loadStuff(){
 
     $("#openSettingMenu").click(function() {
 
+        $(".settingList").removeClass("settingListBolder");
+
         if(count%2==0){
             $('.settingMenu').animate({ marginLeft: '-15%' , opacity: 1 }, 500);
             $(".centralContent").fadeTo( "slow" , 0.2);
-            //$("#bottomNavbar").fadeTo( "slow" , 0);
+            $("#bottomNavbar").fadeTo( "slow" , 0);
             $("#bottomNavbar").addClass("notClickable");
 
             comparsoPrimo=true;
@@ -46,7 +48,7 @@ function loadStuff(){
         else{
             $('.settingMenu').animate({ marginLeft: '1em' , opacity: 1 }, 1000);
             $( ".centralContent" ).fadeTo( "slow" , 1);
-            //$( "#bottomNavbar" ).fadeTo( "slow" , 1);
+            $( "#bottomNavbar" ).fadeTo( "slow" , 1);
             $("#bottomNavbar").removeClass("notClickable");
 
 
@@ -60,19 +62,79 @@ function loadStuff(){
             $(".centralContent").click(function (){
                 $('.settingMenu').animate({ marginLeft: '1em' , opacity: 1 }, 1000);
                 $( ".centralContent" ).fadeTo( "slow" , 1);
-                //$( "#bottomNavbar" ).fadeTo( "slow" , 1);
+                $( "#bottomNavbar" ).fadeTo( "slow" , 1);
                 $("#bottomNavbar").removeClass("notClickable");
                 count++;
                 comparsoPrimo=false;
             });
 
-            $(".settingList").click(function(){
-                // RIMUOVE GLI EVIDENZIATI
-                $(".settingList").removeClass("settingListBolder");
+            //******** MOSTRA IL SOTTOMENU DI "DATI"  **********
+            $("#datiSetting").click(function(){
 
-                // EVIDENZIA QUELLO CLICCATO
+                // RIMUOVE GLI EVIDENZIATI ED EVIDENZIA IL CLICCATO
+                $(".settingList").removeClass("settingListBolder");
                 $(this).addClass("settingListBolder");
-                 $('.settingMenu').animate({ marginLeft: '-40%' , opacity: 1 }, 500);
+
+                // TOGLIE TUTTI I SOTTOMENU
+                $(".child").css("display","none");
+
+                // MOSTRA IL CORRETTO SOTTOMENU
+                $(".childDati").css("display","inline-block");
+
+                //ANIMAZIONE
+                $('.settingMenu').animate({ marginLeft: '-40%' , opacity: 1 }, 500);
+            });
+
+
+            //******** MOSTRA IL SOTTOMENU DI "IMPOSTAZIONI"  **********
+            $("#impostazioniSetting").click(function(){
+
+                // RIMUOVE GLI EVIDENZIATI ED EVIDENZIA IL CLICCATO
+                $(".settingList").removeClass("settingListBolder");
+                $(this).addClass("settingListBolder");
+
+                // TOGLIE TUTTI I SOTTOMENU
+                $(".child").css("display","none");
+
+                // MOSTRA IL CORRETTO SOTTOMENU
+                $(".childImpostazioni").css("display","inline-block");
+
+                // ANIMAZIONE
+                $('.settingMenu').animate({ marginLeft: '-40%' , opacity: 1 }, 500);
+            });
+
+            //******** MOSTRA IL SOTTOMENU DI "LINGUA"  **********
+            $("#linguaSetting").click(function(){
+
+                // RIMUOVE GLI EVIDENZIATI ED EVIDENZIA IL CLICCATO
+                $(".settingList").removeClass("settingListBolder");
+                $(this).addClass("settingListBolder");
+
+                // TOGLIE TUTTI I SOTTOMENU
+                $(".child").css("display","none");
+
+                // MOSTRA IL CORRETTO SOTTOMENU
+                $(".childLingua").css("display","inline-block");
+
+                // ANIMAZIONE
+                $('.settingMenu').animate({ marginLeft: '-40%' , opacity: 1 }, 500);
+            });
+
+            //******** MOSTRA IL SOTTOMENU DI "INFO"  **********
+            $("#infoSetting").click(function(){
+
+                // RIMUOVE GLI EVIDENZIATI ED EVIDENZIA IL CLICCATO
+                $(".settingList").removeClass("settingListBolder");
+                $(this).addClass("settingListBolder");
+
+                // TOGLIE TUTTI I SOTTOMENU
+                $(".child").css("display","none");
+
+                // MOSTRA IL CORRETTO SOTTOMENU
+                $(".childInfo").css("display","inline-block");
+
+                // ANIMAZIONE
+                $('.settingMenu').animate({ marginLeft: '-40%' , opacity: 1 }, 500);
             });
     }
         //TESTO MODIFICABILE :)
