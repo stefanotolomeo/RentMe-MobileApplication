10b59b94fc8b242d68d6377340f1b3cd4d071bc4
stefanotolomeo <ste.tolomeo@gmail.com>
2015-08-05 19:44:06 +0200

    favourite new

--- www/js/favourite.js
+++ www/js/favourite.js
@@ -1,9 +1,170 @@
+
+var count=0;
+var menuVisible=false;
 var opened=false;
+//var menuVisible=false;
+
+$(document).ready(loadStuff);
 
 
-$(document).ready(action);
 
+function loadStuff(){
+    $(".selected").css("color","blue");
 
+    $(".a").click(function(){
+        if(opened==false && menuVisible==false){
+            openDiv(this);
+            disappearOther(this);
+
+        }
+
+        $(this).siblings().find("#contactButton").click(function(){
+            if (opened==true && menuVisible==false){
+                console.log("contatta");
+            }
+        });
+
+        $(this).siblings().find("#closeButton").click(function(){
+            if(opened==true && menuVisible==false){
+                closeDiv(this);
+                appearOther(this);
+            }
+        });
+    });
+
+    $("#openSettingMenu").click(function() {
+        $(".settingList").removeClass("settingListBolder");
+
+        if(count%2==0){
+            menuOpen();
+        }
+        else{
+            menuClose();
+        }
+    });
+
+    // Se schiaccio su centralContent:
+    $(".centralContent").click(function (){
+
+        if(menuVisible==true){
+            console.log("cliccato centralContent")
+            menuClose();
+        }
+    });
+
+    // Se schiaccio sulla duplicated bottom navbar:
+    $("#duplicatedBottomNavbar").click(function (){
+        if(menuVisible==true){
+            console.log("cliccato duplicated")
+            menuClose();
+        }
+    });
+
+    //******** MOSTRA IL SOTTOMENU DI "DATI"  **********
+    $("#datiSetting").click(function(){
+        if(menuVisible==true){
+            // RIMUOVE GLI EVIDENZIATI ED EVIDENZIA IL CLICCATO
+                $(".settingList").removeClass("settingListBolder");
+                $(this).addClass("settingListBolder");
+
+            // TOGLIE TUTTI I SOTTOMENU
+                $(".child").css("display","none");
+
+            // MOSTRA IL CORRETTO SOTTOMENU
+                $(".childDati").css("display","inline-block");
+
+            //ANIMAZIONE
+                $('.settingMenu').animate({ marginLeft: '-40%' , opacity: 1 }, 500);
+        }
+    });
+
+    //******** MOSTRA IL SOTTOMENU DI "IMPOSTAZIONI"  **********
+    $("#impostazioniSetting").click(function(){
+        if(menuVisible==true){
+            // RIMUOVE GLI EVIDENZIATI ED EVIDENZIA IL CLICCATO
+                $(".settingList").removeClass("settingListBolder");
+                $(this).addClass("settingListBolder");
+
+            // TOGLIE TUTTI I SOTTOMENU
+                $(".child").css("display","none");
+
+            // MOSTRA IL CORRETTO SOTTOMENU
+                $(".childImpostazioni").css("display","inline-block");
+
+            // ANIMAZIONE
+                $('.settingMenu').animate({ marginLeft: '-40%' , opacity: 1 }, 500);
+        }
+    });
+
+    //******** MOSTRA IL SOTTOMENU DI "LINGUA"  **********
+    $("#linguaSetting").click(function(){
+        if(menuVisible==true){
+            // RIMUOVE GLI EVIDENZIATI ED EVIDENZIA IL CLICCATO
+                $(".settingList").removeClass("settingListBolder");
+                $(this).addClass("settingListBolder");
+
+            // TOGLIE TUTTI I SOTTOMENU
+                $(".child").css("display","none");
+
+            // MOSTRA IL CORRETTO SOTTOMENU
+                $(".childLingua").css("display","inline-block");
+
+            // ANIMAZIONE
+                $('.settingMenu').animate({ marginLeft: '-40%' , opacity: 1 }, 500);
+        }
+    });
+
+    //******** MOSTRA IL SOTTOMENU DI "INFO"  **********
+    $("#infoSetting").click(function(){
+        if(menuVisible==true){
+            // RIMUOVE GLI EVIDENZIATI ED EVIDENZIA IL CLICCATO
+                $(".settingList").removeClass("settingListBolder");
+                $(this).addClass("settingListBolder");
+
+            // TOGLIE TUTTI I SOTTOMENU
+                $(".child").css("display","none");
+
+            // MOSTRA IL CORRETTO SOTTOMENU
+                $(".childInfo").css("display","inline-block");
+
+            // ANIMAZIONE
+                $('.settingMenu').animate({ marginLeft: '-40%' , opacity: 1 }, 500);
+        }
+    });
+
+}
+
+function menuOpen(){
+    $('.settingMenu').animate({ marginLeft: '-15%' , opacity: 1 }, 500);
+
+    $(".centralContent").fadeTo( "slow" , 0.2);
+
+    // Mostra la duplicated navbar e nasconde quella vera
+    $("#bottomNavbar").css("display","none");
+    $("#duplicatedBottomNavbar").css("display","block");
+
+    count++;
+    menuVisible=true;
+    console.log("menu aperto");
+}
+
+function menuClose(){
+    $('.settingMenu').animate({ marginLeft: '1em' , opacity: 1 }, 1000);
+
+    $( ".centralContent" ).fadeTo( "slow" , 1);
+    $( "#duplicatedBottomNavbar" ).fadeTo( "slow" , 1);
+
+    $("#bottomNavbar").css("display","block");
+    $("#duplicatedBottomNavbar").css("display","none");
+
+    count++;
+    menuVisible=false;
+
+    console.log("menu chiuso");
+
+}
+
+/*
 function action(){
     $(".a").click(function(){
         if(opened==false){
@@ -23,9 +184,9 @@ function action(){
                 closeDiv(this);
                 appearOther(this);
             }
-        });
+        });*/
 
-        /*var prevDiv=$(this).siblings().find("#closeButton");
+/*var prevDiv=$(this).siblings().find("#closeButton");
         $(".a").not(this).click(function(){
             //console.log("altro div");
             if(opened==true){
@@ -33,9 +194,9 @@ function action(){
                 //closeDiv(prevDiv);
                 //return;
             }
-        });*/
+        });
     });
-}
+}*/
 
 function openDiv(x){
 
@@ -78,6 +239,7 @@ function disappearOther(aDiv){
     allOtherDiv1.addClass("divNotClickable");
 
 }
+
 function appearOther(aDiv){
     var allOtherDiv2=$(".preferRow").not($(aDiv).closest(".preferRow"));
 
