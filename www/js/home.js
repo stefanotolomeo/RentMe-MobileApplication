35451aa3aca3fb0d5fb2527ca3645d6b9f0225d6
stefanotolomeo <ste.tolomeo@gmail.com>
2015-08-07 18:35:55 +0200

    z index

--- www/js/home.js
+++ www/js/home.js
@@ -32,7 +32,7 @@ function loadStuff(){
     // Non riesco a passare il parametro da index.js
     // Per il momento non lo passo e fisso io un valore per fare delle prove
     //var idClient =  getUrlParameters("id", "", true);
-/*
+
     var idClient=3;
     //console.log(idClient);
     $.ajax({
@@ -58,9 +58,30 @@ function loadStuff(){
     */
 
 
+function changeNotice(){
+
+}
+
+$(document).ready(start);
+
+function start(){
+    //var myVar = setInterval(function () {changeNotice()}, 2000);
 
 
+    $(".pageTitleDiv").click(function(){
+        //$(".myContent").fadeOut("slow");
 
+        $(".c1").animate({
+            marginLeft: '-18em' ,
+            opacity:0
+        }, 500);
+        //$(".c1").fadeTo("slow",0);
+        $(".c2").fadeTo("slow",1);
+
+
+    });
+
+}
 
 
 
