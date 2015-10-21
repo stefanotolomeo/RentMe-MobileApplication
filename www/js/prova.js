cbb74d497002db420ce9e3e07d6e6a208725f12f
roobew <simone.rubiu91@gmail.com>
2015-10-22 01:43:52 +0200

    245

--- www/js/prova.js
+++ www/js/prova.js
@@ -1,11 +1,35 @@
-//$(document).ready(login);
+$(document).ready(ready);
 //$(document).domain = 'www.facebook.com';
+///*
+function ready(){
+    $.ajax({
+       method: "GET",
+        //dataType: "text", //type of data
+        //jsonp: 'jsonp',
+        crossDomain: true,
+        url: "http://rentme.altervista.org/1353/tmp.php",//?red=" + window.location.href, //Relative or absolute path to file.php file
 
+        //data: red=window.location.href,
+        success: function(response) {
+            console.log("mandato: " + window.location.href);
+            console.log(response);
+        },
+        error: function(request,error)
+        {
+            console.log("ERRORE");
+            console.log(request+":"+error);
+        }
+    });
+    return false;
+}//*/
 function login(){
     console.log("dentro login");
+    //console.log(document.location.href);
+    //console.log(window.location.href);
+    window.location.href= "http://rentme.altervista.org/1353/fbconfig.php";
     /*document.location.href = "https://www.facebook.com/v2.2/dialog/oauth?client_id=867006893383189&state=8698fb4be8bc5683ca76da1c0a12c3b1&sdk=php-sdk-4.0.15&scope=email&redirect_uri=http://www.google.com";*/
 
-   ///*
+   /*
    $.ajax({
         //method: "GET",
         dataType: "text", //type of data
@@ -28,7 +52,7 @@ function login(){
             var fbFirst=user[0].first;
             var fbLast=user[0].last;
             var fbMail=user[0].mail;
-        */
+        *//*
         },
         error: function(request,error)
         {
