ce155b89d89eec2abdea2d571c46a90cf3b0e729
roobew <simone.rubiu91@gmail.com>
2015-10-21 17:32:21 +0200

    dlgbòjhbsl

--- www/js/prova.js
+++ www/js/prova.js
@@ -1,94 +1,34 @@
-
-     // Defaults to sessionStorage for storing the Facebook token
-     openFB.init({appId: '867006893383189'});
-    //  Uncomment the line below to store the Facebook token in localStorage instead of sessionStorage
-    //  openFB.init({appId: 'YOUR_FB_APP_ID', tokenStore: window.localStorage});
-    function login() {
-        openFB.login(
-                function(response) {
-                    if(response.status === 'connected') {
-                        alert('Facebook login succeeded, got access token: ' + response.authResponse.accessToken);
-                    } else {
-                        alert('Facebook login failed: ' + response.error);
-                    }
-                }, {scope: 'email,read_stream,publish_actions'});
-    }
-    function getInfo() {
-        openFB.api({
-            path: '/me',
-            success: function(data) {
-                console.log(JSON.stringify(data));
-                document.getElementById("userName").innerHTML = data.name;
-                document.getElementById("userPic").src = 'http://graph.facebook.com/' + data.id + '/picture?type=small';
-            },
-            error: errorHandler});
-    }
-    function share() {
-        openFB.api({
-            method: 'POST',
-            path: '/me/feed',
-            params: {
-                message: document.getElementById('Message').value || 'Testing Facebook APIs'
-            },
-            success: function() {
-                alert('the item was posted on Facebook');
-            },
-            error: errorHandler});
-    }
-    function readPermissions() {
-        openFB.api({
-            method: 'GET',
-            path: '/me/permissions',
-            success: function(result) {
-                alert(JSON.stringify(result.data));
-            },
-            error: errorHandler
-        });
-    }
-    function revoke() {
-        openFB.revokePermissions(
-                function() {
-                    alert('Permissions revoked');
-                },
-                errorHandler);
-    }
-    function logout() {
-        openFB.logout(
-                function() {
-                    alert('Logout successful');
-                },
-                errorHandler);
-    }
-    function errorHandler(error) {
-        alert(error.message);
-    }
-
-
 //$(document).ready(login);
 //$(document).domain = 'www.facebook.com';
 
-/*function login(){
+function login(){
     console.log("dentro login");
+    /*document.location.href = "https://www.facebook.com/v2.2/dialog/oauth?client_id=867006893383189&state=8698fb4be8bc5683ca76da1c0a12c3b1&sdk=php-sdk-4.0.15&scope=email&redirect_uri=http://www.google.com";*/
 
-    /*$.ajax({
-        method: "POST",
-        //dataType: "json", //type of data
+   ///*
+   $.ajax({
+        //method: "GET",
+        dataType: "text", //type of data
+        //jsonp: 'jsonp',
         crossDomain: true,
         url: "http://rentme.altervista.org/1353/fbconfig.php", //Relative or absolute path to file.php file
         //url : "http://hypermediabiggym.altervista.org/Location/getLocation.php",
         //data: {email:emailLogin , pw:pwLogin},
         success: function(response) {
             console.log("dentro ajax");
-            //  console.log(response);
-            var loca=JSON.parse(response);
-            $(".prova").html(loca[0].prova);
 
-          /*  var fbId=user[0].id;
+            //window.location.href = "http://www.google.com";//response + "&redirect_uri=http://127.0.0.1:41103/www/index.html";
+             console.log(response);
+            document.location.href =response;
+            //var loca=JSON.parse(response);
+
+            //$(".prova").html(loca[0].prova);
+
+        /*  var fbId=user[0].id;
             var fbFirst=user[0].first;
             var fbLast=user[0].last;
             var fbMail=user[0].mail;
-
-//*//*
+        */
         },
         error: function(request,error)
         {
@@ -96,9 +36,9 @@
             console.log(request+":"+error);
         }
     });
-    *//*
+    //*/
     console.log("fine login");
     return false;
-}*/
+}
 
 
